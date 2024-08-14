2710ce441226a2cd5c55ca2ca9039e2d5b67fda4
Joseph Liu <102351230+Ensyllis@users.noreply.github.com>
2024-08-13 21:12:53 -0700

    Update route.ts

--- src/app/api/readCSV/route.ts
+++ src/app/api/readCSV/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
-import fs from 'fs';
+import { NextResponse, NextRequest } from 'next/server';
+import { promises as fs } from 'fs';
 import path from 'path';
 import { parse } from 'csv-parse/sync';
 
